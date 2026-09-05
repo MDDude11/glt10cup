d2e6ac1c46e20fe5a8124bf9dcba2051ee34ba3f
MDDude11 <88928990+MDDude11@users.noreply.github.com>
2026-09-05 21:17:53 +0800

    Add Rollup input options for multiple HTML files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/glt10cup/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        portal: resolve(__dirname, 'portal.html'),
+      },
+    },
+  },
 })
